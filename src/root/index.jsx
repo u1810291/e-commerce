bc6e49a413221795bcf46f95bb05bbd0c1c931f8
u1810291 <o.rizakulov@student.inha.uz>
2021-04-12 16:31:49 +0500

    apps js configurations

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,30 +1,8 @@
 /* eslint-disable react/react-in-jsx-scope */
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import App from '../container/App';
 
-export default App;
+export default () => (
+  <App />
+);
