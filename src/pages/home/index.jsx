13a7d610f455616c72296ad431b2649607ebf55f
u1810291 <otabekbutcher@gmail.com>
2021-12-29 00:07:50 +0500

    feat: added home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,53 +1,20 @@
 import React from 'react';
-import { withRouter } from 'next/router';
 import Heading from '../../container/Head';
 import Body from '../../container/Body';
 import Footer from '../../container/Footer';
-import products from '../../services/products';
 
-function Home({ data }) {
+function Home() {
   return (
     <>
       <Heading title="Online shopping" />
       <Body>
         <div>
-          {data && data.map((el) => (
-            <div key={el.id}>
-              <span>
-                {el.id}
-              </span>
-              <span>
-                {el.category}
-              </span>
-              <span>
-                {el.image}
-              </span>
-              <span>
-                {el.price}
-              </span>
-              <span>
-                {el.rating.count}
-              </span>
-              <span>
-                {el.rating.rate}
-              </span>
-              <span>
-                {el.title}
-              </span>
-              <span>
-                {el.description}
-              </span>
-            </div>
-          ))}
+          Home
         </div>
       </Body>
       <Footer />
     </>
   );
 }
-export const getServerSideProps = async () => {
-  const { data } = await products.getProducts();
-  return { props: { data } };
-};
 
-export default withRouter(Home);
+export default Home;
