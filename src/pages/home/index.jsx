48850eb3dac0c6670a22a72feb4f14da3b41c985
speedym282 <a.rizakulov@student.inha.uz>
2022-01-17 15:53:34 +0500

    add radio component in home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,6 +5,8 @@ import Footer from '../../container/Footer';
 import Container from '../../components/Container';
 import Checkboxes from '../../components/Forms/CheckBoxes';
 import Inputs from '../../components/Forms/Inputs';
+import Radios from '../../components/Forms/Radios';
+import fedEx from '../../static/assets/svg/FedEx.svg';
 import style from './style.module.scss';
 
 function Home() {
@@ -23,6 +25,9 @@ function Home() {
           <Inputs size="small" label="Hello" placeholder="World" />
           <Inputs size="medium" label="Hello" placeholder="World" />
           <Inputs size="large" label="Hello" placeholder="World" />
+          <Radios label="ME" src={fedEx} size="large" childText={{ type: 'success', price: '+25 USD', txt: 'Additional Price' }} />
+          <Radios label="ME" src={fedEx} size="large" childText={{ type: 'success', price: '', txt: '' }} />
+          <Radios label="ME" src={fedEx} size="small" />
         </div>
         <Container height={300} width={300} bgcolor="#cfe8fc">
           Home
